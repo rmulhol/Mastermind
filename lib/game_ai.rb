637a2019b6b9eea65ca36188cbf4173868ffb39f
Robert Mulholand <robmulholand@gmail.com>
2014-09-22 11:18:55 -0500

    incorporate working game_ai class

--- lib/game_ai.rb
+++ lib/game_ai.rb
@@ -6,9 +6,58 @@ class GameAI
     @number_of_rows = 4
   end
 
-  def generate_all_possible_combinations
+  def generate_all_combinations
     colors_array = (1..number_of_colors).to_a
     possible_combinations = colors_array.repeated_permutation(number_of_rows)
     possible_combinations.to_a
+  end
+
+  def generate_a_guess(remaining_combinations)
+    amount_of_remaining_combinations = remaining_combinations.length
+    remaining_combinations[rand(amount_of_remaining_combinations)]
+  end
+
+  def same_color_same_position(first_guess, second_guess)
+    black_pegs = 0
+    first_guess.each_with_index do |peg, position|
+      black_pegs += 1 if peg == second_guess[position]
+    end
+    black_pegs
+  end
+
+  def same_color(first_guess, second_guess)
+    same_color = 0
+    guess_two = second_guess.clone
+    first_guess.each do |peg|
+      if guess_two.include? peg
+        same_color += 1
+        peg_to_remove = guess_two.index(peg)
+        guess_two.delete_at(peg_to_remove)
+      end
+    end
+    same_color
+  end
+
+  def get_feedback(first_guess, second_guess)
+    black_pegs = same_color_same_position(first_guess, second_guess)
+    total_same_color = same_color(first_guess, second_guess)
+    white_pegs = total_same_color - black_pegs
+    result_of_comparison = [black_pegs, white_pegs]
+    result_of_comparison
+  end
+
+  def is_a_possible_combination?(previous_guess, feedback, remaining_option)
+    feedback_on_remaining_option = get_feedback(previous_guess, remaining_option)
+    feedback_on_remaining_option == feedback
+  end
+
+  def reduce_remaining_combinations(previous_guess, feedback, remaining_combinations)
+    new_set_of_combinations = []
+    remaining_combinations.each do |possible_combination|
+      if is_a_possible_combination?(previous_guess, feedback, possible_combination)
+        new_set_of_combinations << possible_combination
+      end
+    end
+    new_set_of_combinations
   end
 end
--- spec/game_ai_spec.rb
+++ spec/game_ai_spec.rb
@@ -3,8 +3,8 @@ require 'game_ai'
 describe GameAI do
   let(:new_game) { described_class.new }
 
-  describe "#generate_all_possible_combinations" do
-    let(:all_combinations) { new_game.generate_all_possible_combinations }
+  describe "#generate_all_combinations" do
+    let(:all_combinations) { new_game.generate_all_combinations }
 
     it "creates an array" do
       expect(all_combinations.class).to eq(Array)
@@ -24,6 +24,159 @@ describe GameAI do
 
     it "creates an array of arrays that are each unique" do
       expect(all_combinations.uniq.length).to eq(1296)
+    end
+  end
+
+  describe "#generate_a_guess" do
+    let(:possible_combinations) { new_game.generate_all_combinations }
+    let(:new_guess) { new_game.generate_a_guess(possible_combinations) }
+
+    it "outputs an array" do
+      expect(new_guess.class).to eq(Array)
+    end
+
+    it "outputs an array of length 4" do
+      expect(new_guess.length).to eq(4)
+    end
+
+    it "outputs an array of 4 numbers" do
+      expect(new_guess.map { |element| element.is_a? Fixnum }).not_to include(false)
+    end
+
+    it "outputs an array from the argument that is passed in" do
+      expect(possible_combinations.include? new_guess).to eq(true)
+    end
+
+    it "outputs a random array from the argument that is passed in" do
+      #doesn't truly new_game randomness but gets very close
+      guess_one = new_game.generate_a_guess(possible_combinations)
+      guess_two = new_game.generate_a_guess(possible_combinations)
+      guess_three = new_game.generate_a_guess(possible_combinations)
+      expect(guess_one == guess_two && guess_two == guess_three).to eq(false)
+    end
+  end
+
+  describe '#same_color_same_position' do
+    it "correctly identifies 0 black pegs when all pegs are different" do
+      expect(new_game.same_color_same_position([1, 1, 1, 1], [2, 2, 2, 2])).to eq(0)
+    end  
+
+    it "correctly identifies 1 black peg when the first pegs are both 1" do
+      expect(new_game.same_color_same_position([1, 1, 1, 1], [1, 2, 2, 2])).to eq(1)
+    end
+    
+    it "correctly identifies 1 black peg when the second pegs are both 1" do
+      expect(new_game.same_color_same_position([1, 1, 1, 1], [2, 1, 2, 2])).to eq(1)
+    end
+    
+    it "correctly identifies 2 black pegs when the first and second pegs are both 1" do
+      expect(new_game.same_color_same_position([1, 1, 2, 2], [1, 1, 1, 1])).to eq(2)
+    end
+
+    it "correctly identifies 4 black pegs when all pegs are 6" do
+      expect(new_game.same_color_same_position([6, 6, 6, 6], [6, 6, 6, 6])).to eq(4)
+    end
+  end
+
+  describe "#same_color" do
+    it "correctly identifies 0 same-color pegs when all pegs are different" do
+      expect(new_game.same_color([1, 1, 1, 1], [2, 2, 2, 2])).to eq(0)
+    end
+
+    it "correctly identifies 1 same-color peg" do
+      expect(new_game.same_color([1, 2, 2, 2], [1, 1, 1, 1])).to eq(1)
+    end
+
+    it "correctly identifies 1 same-color peg when the first array contains three of an element and the second array contains one" do
+      expect(new_game.same_color([1, 1, 1, 3], [1, 2, 2, 2])).to eq(1)
+    end
+    
+    it "correctly identifies 2 same-color pegs" do
+      expect(new_game.same_color([3, 3, 1, 1], [3, 3, 3, 3])).to eq(2)
+    end
+
+    it "correctly identifies 2 same-color pegs when the first array contains one of an element and the second array contains three" do
+      expect(new_game.same_color([1, 1, 1, 2], [1, 2, 2, 2])).to eq(2)
+    end
+
+    it "correctly identifies 3 same-color pegs" do
+      expect(new_game.same_color([1, 1, 1, 2], [1, 1, 1, 1])).to eq(3)
+    end
+
+    it "correctly identifies 3 same-color pegs when the first array contains one of an element and the second array contains two" do
+      expect(new_game.same_color([1, 1, 1, 2], [1, 2, 2, 1])).to eq(3)
+    end
+    
+    it "correctly identifies 4 same-color pegs when all pegs are the same" do
+      expect(new_game.same_color([1, 1, 1, 1], [1, 1, 1, 1])).to eq(4)
+    end
+  end
+
+  describe "#get_feedback" do
+    it "correctly identifies 0 black pegs and 0 same color pegs" do
+      expect(new_game.get_feedback([1, 1, 1, 1], [2, 2, 2, 2])).to eq([0, 0])
+    end
+
+    it "correctly identifies 1 black peg and 0 same color pegs" do
+      expect(new_game.get_feedback([1, 1, 1, 1], [1, 2, 2, 2])).to eq([1, 0])
+    end
+
+    it "correctly identifies 2 black pegs and 0 same color pegs" do
+      expect(new_game.get_feedback([1, 1, 1, 1], [1, 1, 2, 2])).to eq([2, 0])
+    end
+
+    it "correctly identifies 0 black pegs and 1 same color peg" do
+      expect(new_game.get_feedback([2, 2, 1, 2], [1, 3, 3, 3])).to eq([0, 1])
+    end
+
+    it "correctly identifies 0 black pegs and 2 same color pegs" do
+      expect(new_game.get_feedback([2, 1, 1, 2], [3, 2, 2, 3])).to eq([0, 2])
+    end
+
+    it "correctly identifies 1 black peg and 1 white peg" do
+      expect(new_game.get_feedback([1, 2, 3, 4], [1, 5, 6, 2])).to eq([1, 1])
+    end
+
+    it "correctly identifies 2 black pegs and 2 white pegs" do
+      expect(new_game.get_feedback([1, 2, 3, 4], [2, 1, 3, 4])).to eq([2, 2])
+    end
+  end
+
+  describe "#is_a_possible_combination" do
+    it "keeps possible combination with four black pegs and four of the same pegs" do
+      expect(new_game.is_a_possible_combination?([1, 1, 1, 1], [4, 0], [1, 1, 1, 1])).to be_truthy
+    end
+
+    it "rejects possible combination with four black pegs and four different pegs" do
+      expect(new_game.is_a_possible_combination?([1, 1, 1, 1], [4, 0], [2, 2, 2, 2])).to be_falsey
+    end
+
+    it "keeps possible combination with three black pegs and three shared pegs" do
+      expect(new_game.is_a_possible_combination?([1, 1, 1, 3], [3, 0], [1, 1, 1, 2])).to be_truthy
+    end
+
+    it "rejects possible combination with three black pegs but only two shared pegs" do
+      expect(new_game.is_a_possible_combination?([1, 1, 1, 3], [3, 0], [1, 1, 2, 2])).to be_falsey
+    end
+  end
+
+  describe "#reduce_remaining_combinations" do
+    it "correctly reduces to one combination with 4 black pegs and four 1 pegs" do
+      possible_combinations = new_game.generate_all_combinations
+      expect(new_game.reduce_remaining_combinations([1, 1, 1, 1], [4, 0], possible_combinations)).to eq([[1, 1, 1, 1]])
+    end
+
+    it "correctly reduces to one combination with 4 black pegs and four 2 pegs" do
+      possible_combinations = new_game.generate_all_combinations
+      expect(new_game.reduce_remaining_combinations([2, 2, 2, 2], [4, 0], possible_combinations)).to eq([[2, 2, 2, 2]])
+    end
+
+    it "correctly reduces to two combinations with 3 black pegs" do
+      expect(new_game.reduce_remaining_combinations([1, 1, 1, 1], [3, 0], [[1, 1, 1, 2], [1, 1, 1, 3], [1, 2, 3, 4]])).to eq([[1, 1, 1, 2], [1, 1, 1, 3]])
+    end
+
+    it "correctly reduces to one combination with 1 black peg" do
+      expect(new_game.reduce_remaining_combinations([1, 1, 1, 1], [1, 0], [[1, 1, 1, 2], [1, 1, 1, 3], [1, 2, 3, 4]])).to eq([[1, 2, 3, 4]])
     end
   end
 end
