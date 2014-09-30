8c7a4c63d51e3ae58df18a8ce3456f4eb04c66b0
Robert Mulholand <robmulholand@gmail.com>
2014-09-30 15:38:14 -0500

    improve command line display

--- lib/cl_display.rb
+++ lib/cl_display.rb
@@ -8,15 +8,15 @@ class CommandLineDisplay
   end
 
   def welcome_user
-    "Hello, welcome to Mastermind.\n"
+    "\nHello, welcome to Mastermind.\n\n"
   end
 
   def explain_game
     "In this game, you'll think up a secret code and I'll try to figure it out.\n"\
-"Your code should be 4 elements long, with each element drawn from the following six colors:\n"\
-"red, blue, green, yellow, purple, orange. Repeats are allowed. After each guess, I'll ask for feedback.\n"\
-"You'll need to tell me how many of my picks are the correct color and in the correct position, as well as\n"\
-"how many are the correct color but in the incorrect position. Hit enter when you're ready to play!\n"
+"Your code should be 4 elements long, with each element drawn from the following six colors: "\
+"red, blue, green, yellow, purple, orange. Repeats are allowed.\nAfter each guess, I'll ask for feedback. "\
+"You'll need to tell me how many of my picks are the correct color and in the correct position, as well as "\
+"how many are the correct color but in the incorrect position.\nHit enter when you're ready to play!\n"
   end
 
   def offer_first_guess(guess)
@@ -24,38 +24,38 @@ class CommandLineDisplay
   end
 
   def solicit_feedback_on_black_pegs
-    "How many of my picks are the correct color *and* in the correct position?\n"
+    "\nHow many of my picks are the correct color *and* in the correct position?\n"
   end
 
   def solicit_feedback_on_white_pegs
-    "How many of my picks are the correct color by in the incorrect position?\n"
+    "\nHow many of my picks are the correct color by in the incorrect position?\n"
   end
 
   def offer_next_guess(guess)
-    "My next guess is #{guess}\n"
+    "\nMy next guess is #{guess}\n"
   end
 
   def error_message
-    "Invalid input. Please try again.\n"
+    "\nInvalid input. Please try again.\n"
   end
 
   def error_message_for_invalid_aggregate_feedback
-    "That feedback isn't possible. Try again.\n"
+    "\nThe aggregate feedback you've provided isn't possible. Try again.\n"
   end
 
   def no_combinations_error
-    "It seems there are no remaining possible combinations. Would you like to restart from the beginning? (y/n)\n"
+    "\nIt seems there are no remaining possible combinations. Would you like to restart from the beginning? (y/n)\n"
   end
 
   def restart_game
-    "OK, starting from the beginning!\n\n"
+    "\nOK, starting from the beginning ...\n"
   end
 
   def goodbye
-    "OK, hope to play again soon!\n"
+    "\nOK, hope to play again soon!\n"
   end
 
   def announce_win(turns)
-    "I figured out your code in #{turns} turns!"
+    "\nI figured out your code in #{turns} turns!\n"
   end
 end
